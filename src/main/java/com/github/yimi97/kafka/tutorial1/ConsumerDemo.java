b55f2b42daec1d9be597abbf04355cb53dd73380
yimi97 <msif1900@gmail.com>
2020-06-03 21:51:40 -0400

    Producerkeys and Consumer

---
+++ src/main/java/com/github/yimi97/kafka/tutorial1/ConsumerDemo.java
@@ -0,0 +1,68 @@
+package com.github.yimi97.kafka.tutorial1;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.protocol.types.Field;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class ConsumerDemo {
+    public static void main(String[] args) {
+
+        Logger logger = LoggerFactory.getLogger(ConsumerDemo.class.getName());
+
+        String bootstrapServers = "vcm-15078.vm.duke.edu:9092";
+        String groupId = "my-fourth-application";
+        String topic = "first_topic";
+
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+
+        /*
+        * StringDeserializer: when a producer creates a string and serialize to bytes and sends it to Kafka,
+        * and when Kafka sends theses bytes right back to our consumer, our consumer has to take these bytes
+        * and create a string from it,and that process is call deserialization
+        * */
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+
+        /*
+        * "earliest": read from the very beginning of the topic
+        * "latest": read only the new msg
+        * "none": throw an error
+        * */
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // create consumer
+        KafkaConsumer<String, String> consumer =
+                new KafkaConsumer<String, String>(properties);
+
+        // subscribe consumer to our topics
+        // Collections.singleton: only subscribe to one topic
+        // could subscribe more by using Arrays.asList("first_topic", "second_topic")
+        consumer.subscribe(Collections.singleton(topic));
+
+        // poll for new data
+        while (true) {
+            ConsumerRecords<String, String> records =
+                    consumer.poll(Duration.ofMillis(100)); // poll: the non-deprecated version uses a duration
+
+            for (ConsumerRecord<String, String> record: records) {
+                logger.info("Key: " + record.key() + ", Value: " + record.value());
+                logger.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+
+            }
+
+        }
+
+    }
+}
--- src/main/java/com/github/yimi97/kafka/tutorial1/ProducerDemoKeys.java
+++ src/main/java/com/github/yimi97/kafka/tutorial1/ProducerDemoKeys.java
@@ -1,14 +1,17 @@
 package com.github.yimi97.kafka.tutorial1;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 public class ProducerDemoKeys {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        final Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallback.class);
+
         String bootstrapServers = "vcm-15078.vm.duke.edu:9092";
         // create producer properties
         Properties properties = new Properties();
@@ -19,16 +22,48 @@ public class ProducerDemoKeys {
         // create the producer
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
 
-        // create a producer record
-        ProducerRecord<String, String> record =
-                new ProducerRecord<String, String>("first_topic", "Hello world!");
+        for (int i=0; i<10; i++) {
+            // create a producer record
 
-        // send data
-        producer.send(record);
+            String topic = "first_topic";
+            String value = "hello world" + Integer.toString(i);
+            String key = "id_" + Integer.toString(i);
 
+            final ProducerRecord<String, String> record =
+                    new ProducerRecord<String, String>(topic, key, value);
+
+            logger.info("Key: " + key); // log the key
+            // id_0 is going to partition 1
+            // id_1 is going to partition 0
+            // ...
+            // With keys, running several time, we guarantee the same key will always go to same partition.
+            // In practice, it will be the track id
+
+            // send data
+            producer.send(record, new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    // executes every time a record is successfully sent or an exception is thrown
+                    if (e == null) {
+                        // the record is successfully sent
+                        logger.info("Received new metadata. \n" +
+                                "Topic: " + recordMetadata.topic() + '\n' +
+                                "Partition: " + recordMetadata.partition() + "\n" +
+                                "Offset: " + recordMetadata.offset() + "\n" +
+                                "Timestamp: " + recordMetadata.timestamp());
+                    } else {
+                        logger.error("Error while producing", e);
+                    }
+                }
+            }).get();  // block the .send() to make it synchronous - don't do this in production! Just explain here.
+            // add throws ExecutionException and InterruptedException to main
+        }
         // flush data
         producer.flush();
         producer.close();
 
     }
 }
+/*
+* What if only one partition exists?
+*
+* */
